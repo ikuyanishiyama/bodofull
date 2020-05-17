b0422269c69216d5b2db37adb42d0cc143b4a377
Ikuya Nishiyama <n_ikuya2136kh@outlook.jp>
2020-05-17 02:28:10 +0000

    アカウント登録の完了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,29 @@
 class UsersController < ApplicationController
-  def new
+  
+  def show
+    @user = User.find(params[:id])
   end
+  
+  def new
+    @user = User.new
+  end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "アカウントの登録に成功しました"
+      redirect_to @user
+    else 
+      render "new"
+    end
+  end
+  
+  private
+    
+    # ストロングパラメーター
+    def user_params
+      # user属性を必須とし、name,email,password属性を許可する
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
 end
